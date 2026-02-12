65afbf2bba78fc22aa3444cb574888e767af5adb
chennachandrika <chennachandrikapadmashali@gmail.com>
2026-02-12 12:22:50 +0530

    feat: auth button text

--- src/components/auth/auth-button.tsx
+++ src/components/auth/auth-button.tsx
@@ -56,7 +56,7 @@ export function AuthButton({ className }: AuthButtonProps) {
 					className={clsx('gap-2', className)}
 				>
 					<LogIn className="h-4 w-4" />
-					<span>Sign In</span>
+					<span>Sign In.</span>
 				</Button>
 
 				<LoginModal
